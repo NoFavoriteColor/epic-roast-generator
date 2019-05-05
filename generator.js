6a257b31accb298878bbd65ddfbb45e1ae1652a2
Sebastian Angeltvedt <sebastian.angeltvedt@gmail.com>
2019-05-05 20:23:56 +0200

    1.0.0

---
+++ generator.js
@@ -0,0 +1,19 @@
+var adjectivesNO = ["slemme", "dumme", "lille", "store", "idiotiske", "feite", "stygge", "demente", "demoniske"]
+var nounsNO = ["kylling", "maur", "slemming", "dumming", "idiot", "imbesil", "kamel", "reptil", "stygging", "demon", ]
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// for (i = 0; i < 75; i++) {
+// document.write("<br> Din " + adjectivesNO[getRandomInt(0, adjectivesNO.length - 1)] + " " + nounsNO[getRandomInt(0, nounsNO.length - 1)])
+// }
+
+adjectivesNO.forEach(function(a) {
+    nounsNO.forEach(function(n) {
+        document.write("<br> Din " + a + " " + n)
+    });
+});
